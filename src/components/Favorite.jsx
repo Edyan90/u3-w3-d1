58021efe841c4ff44504bc73543b8462e34de1bf
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-07-08 22:50:29 +0200

    Initial commit

---
+++ src/components/Favorite.jsx
@@ -0,0 +1,51 @@
+import { Button, Col, Container, ListGroup, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Link, useNavigate } from "react-router-dom";
+import { House, Trash } from "react-bootstrap-icons";
+
+const Favorite = () => {
+  const dispatch = useDispatch();
+  const favorite = useSelector((state) => state.favorite.content);
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <ListGroup className="my-4">
+        <ListGroup.Item variant="primary" className="d-flex justify-content-between align-items-center">
+          <span>Total Favorite Jobs: {favorite.length}</span>
+          <Button onClick={() => navigate("/")}>
+            <House />
+          </Button>
+        </ListGroup.Item>
+      </ListGroup>
+      <h1>Favorite Companies:</h1>
+      {favorite.map((favorite, index) => {
+        return (
+          <Row key={index} className="mx-0 mt-3 p-3" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
+            <Col xs={3}>
+              <Link to={`/${favorite.company_name}`}>{favorite.company_name}</Link>
+            </Col>
+            <Col xs={8}>
+              <a href={favorite.url} target="_blank" rel="noreferrer">
+                {favorite.title}
+              </a>
+            </Col>
+            <Col xs={1}>
+              <Trash
+                onClick={() => {
+                  dispatch({
+                    type: "REMOVE_FROM_FAVORITE",
+                    payload: index,
+                  });
+                }}
+                fill="red"
+              />
+            </Col>
+          </Row>
+        );
+      })}
+    </Container>
+  );
+};
+
+export default Favorite;
---
+++ src/components/Job.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import { HeartFill } from "react-bootstrap-icons";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
+const Job = ({ data }) => {
+  const [favorite, setFavorite] = useState(false);
+
+  // questa variabile la potremo chiamare per attivare il processo di SCRITTURA nello Store
+  const dispatch = useDispatch();
+
+  //  // useSelector ci chiede una funzione per selezionare una porzione di stato, quello che ritorna la funzione sarà il dato trovato in quel valore di stato,
+  // // che verrà prelevato e assegnato alla variabile associata
+  // const favorite = useSelector(state => state.favorite.content);
+
+  return (
+    <Row className="mx-0 mt-3 p-3" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={8}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+      <Col xs={1}>
+        <HeartFill
+          onClick={() => {
+            setFavorite(!favorite),
+              dispatch({
+                type: "ADD_TO_FAVORITE",
+                payload: data,
+              });
+          }}
+          fill={favorite ? "red" : "lightgrey"}
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default Job;
